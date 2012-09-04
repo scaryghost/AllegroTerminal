af64417ee80747251fcf908489eb7e1d1f4db48d
Eric Tsai <thegod0fde4th@gmail.com>
2012-09-03 23:02:28 -0700

    Implemented the rest of the Console class

--- Console.h
+++ Console.h
@@ -18,13 +18,19 @@ public:
     void scrollUp(int numLines);
     void scrollDown(int numLines);
 
-private:
-    int calcNumVisibleLines();
+protected:
+    int calcNumLines(int left, int right);
+    void increment(int &i);
+    void increment(int &i, int offset);
+    void decrement(int &i, int offset);
 
+private:
     int maxVisibleLines;
     int charPerLine;
     int begin;
     int end;
+    int visibleBegin;
+    int visibleEnd;
     std::vector<std::string> lines;
 
 };  //class Console
--- src/Console.cpp
+++ src/Console.cpp
@@ -8,7 +8,8 @@ using std::string;
 
 Console::Console(int charPerLine, int maxLinesStored, int maxVisibleLines) : 
     charPerLine(charPerLine), lines(maxLinesStored),
-    maxVisibleLines(maxVisibleLines), begin(0), end(0) {
+    maxVisibleLines(maxVisibleLines), begin(0), end(0),
+    visibleBegin(0), visibleEnd(0) {
 }
 
 Console::~Console() {
@@ -16,34 +17,65 @@ Console::~Console() {
 
 vector<string> Console::getVisibleLines() const {
     vector<string> visibleLines(calcNumVisibleLines());
-    bool terminate= (begin == end);
-    int vlIndex= 0;
+    bool terminate= (visibleBegin == visibleEnd);
+    int vlIndex= 0, i= visibleBegin;
     
     while(!terminate) {
         visibleLines[vlIndex]= lines[i];
-        i= (i + 1) % lines.size();
+        increment(i);
         vlIndex++;
-        terminate= (i <= end);
+        terminate= (i <= visibleEnd);
     }
     return visibleLines;
 }
 
 void Console::addLine(const string& line) {
-    end= (end + 1) % lines.size();
+    if (visibleEnd == end) {
+        increment(visibleEnd);
+    }
+
+    increment(end);
     lines[end]= line;
     
-    if (calcNumVisibleLines() > maxVisibleLines)
-        begin= (begin + 1) % lines.size();
+    if (begin == end) {
+        increment(begin);
+    }
+    if (calcNumLines(visibleBegin, visibleEnd) > maxVisibleLines) {
+        increment(visibleBegin);
+    }
 }
 
 void Console::scrollUp(int numLines) {
+    int diff= calcNumLines(begin, visibleBegin) - 1;
+    int usedOffset= diff < numLines ? diff : numLines;
+
+    decrement(visibleBegin, usedOffset);
+    decrement(visibleEnd, usedOffset);
 }
 
 void Console::scrollDown(int numLines) {
+    int diff= calcNumLines(visibleEnd, end) - 1;
+    int usedOffset= diff < numLines ? diff : numLines;
+
+    increment(visibleBegin, usedOffset);
+    increment(visibleEnd, usedOffset);
 }
 
-int Console::calcNumVisibleLines() {
-    return (end - start + lines.size()) % lines.size() + 1;
+int Console::calcNumLines(int left, int right) {
+    int max= lines.size();
+    return (right - left + max) % max + 1;
+}
+
+void Console::increment(int &i) {
+    i= (i + 1) % lines.size();
+}
+
+void Console::increment(int &i, int offset) {
+    i= (i + offset) % lines.size();
+}
+
+void Console::decrement(int &i, int offset) {
+    i= (i - offset + lines.size()) % lines.size();
 }
 
 }
