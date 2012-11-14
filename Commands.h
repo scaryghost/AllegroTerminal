0e4349d966a5819c29ff63de1e331011a436064b
etsai <thegod0fde4th@gmail.com>
2012-11-14 02:53:53 -0800

    Implemented the Commands class

--- Commands.h
+++ Commands.h
@@ -2,6 +2,7 @@
 #define ETSAI_ALLEGROTERMINAL_COMMANDS
 
 #include <functional>
+#include <stdexcept>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -17,8 +18,8 @@ class Commands {
 public:
     typedef std::function<void (const std::vector<std::string> &args)> TermCommand;
 
-    static void add(const TermCommand &comm);
-    static void exec(const std::vector<std::string> argv);
+    static void add(std::string name, TermCommand &comm) throw(std::runtime_error);
+    static void exec(const std::string line) throw(std::runtime_error);
 
 private:
     static std::unordered_map<std::string, TermCommand> commands;
---
+++ src/Commands.cpp
@@ -0,0 +1,71 @@
+#include "AllegroTerminal/Commands.h"
+
+#include <sstream>
+#include <utility>
+
+namespace etsai {
+namespace allegroterminal {
+
+using std::make_pair;
+using std::runtime_error;
+using std::string;
+using std::stringstream;
+using std::unordered_map;
+using std::vector;
+
+unordered_map<string, Commands::TermCommand> Commands::commands;
+
+void Commands::add(std::string name, TermCommand &comm) throw(std::runtime_error) {
+    if (commands.count(name) != 0) {
+        stringstream msg(stringstream::out);
+
+        msg << "Command (" << name << ") already registered";
+        throw runtime_error(msg.str().c_str());
+    }
+    commands.insert(make_pair(name, comm));
+}
+
+void Commands::exec(const std::string line) throw(std::runtime_error) {
+    bool findName= true, quoteMode= false;
+    string name, temp;
+    vector<string> args;
+    size_t i;
+
+    /** parse the command line */
+    for(i= 0; i < line.size(); i++) {
+        if (line[i] == '\"') {
+            quoteMode= !quoteMode;
+        } else if (line[i] != ' ' || quoteMode) {
+            temp+= line[i];
+        } else {
+            if (findName) {
+                name= temp;
+                findName= false;
+            } else {
+                args.push_back(temp);
+            }
+            temp.clear();
+        }
+    }
+    if (quoteMode) {
+        throw runtime_error("Unmatched quotation \"");
+    }
+    if (!temp.empty()) {
+        if (findName) {
+            name= temp;
+        } else {
+            args.push_back(temp);
+        }
+    }
+
+    if (commands.count(name) == 0) {
+        stringstream msg(stringstream::out);
+
+        msg << "Unrecognized command: " << name;
+        throw runtime_error(msg.str().c_str());
+    }
+    commands[name](args);
+}
+
+}   //namespace allegroterminal
+}   //namespace etsai
